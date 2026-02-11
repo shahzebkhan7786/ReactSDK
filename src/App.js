35a8dc3670bc322d258e6bddcc78594f9a734d4d
shahzebkhan7786 <53787072+shahzebkhan7786@users.noreply.github.com>
2026-02-11 18:26:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import DualRoomManager from "./DualRoomManager";
 export default function App() {
   return (
     <div style={{ padding: 30 }}>
-      <h1>VideoSDK Relay Demo</h1>
+      <h1>VideoSDK Room Relay Demo</h1>
       <DualRoomManager />
     </div>
   );
