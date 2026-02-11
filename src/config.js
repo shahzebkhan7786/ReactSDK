2a1c35b9462305083b2255f9fa522e58b3bc257b
shahzebkhan7786 <53787072+shahzebkhan7786@users.noreply.github.com>
2026-02-11 19:03:18 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const TOKEN = "PASTE_YOUR_VIDEOSDK_TOKEN_HERE";
+export const TOKEN = "8a57ed8f-8d6c-4893-97d4-fa50814a4837";
 
 export const ROOM_A = "room-a-demo";
 export const ROOM_B = "room-b-demo";
