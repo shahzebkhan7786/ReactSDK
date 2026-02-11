024a7e659cf9cf5cd0313e21864bdd2c39cd1bd2
shahzebkhan7786 <53787072+shahzebkhan7786@users.noreply.github.com>
2026-02-11 19:09:24 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const TOKEN = "8a57ed8f-8d6c-4893-97d4-fa50814a4837";
+export const TOKEN = "7afb3474-3455-49df-8a00-f9d6cb3b6d1a";
 
 export const ROOM_A = "room-a-demo";
 export const ROOM_B = "room-b-demo";
