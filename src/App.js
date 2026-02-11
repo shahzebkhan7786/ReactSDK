63bac94c2284a1c8195b09ef82ba22f9834267f6
shahzebkhan7786 <53787072+shahzebkhan7786@users.noreply.github.com>
2026-02-11 16:13:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import React, { useState } from "react";
-import { MeetingProvider } from "@videosdk.live/react-sdk";
-import Room from "./Room";
-import { TOKEN } from "./config";
+import React from "react";
+import DualRoomManager from "./DualRoomManager";
 
 export default function App() {
-  const [start, setStart] = useState(false);
-
   return (
-    <>
-      {!start ? (
-        <div style={{ padding: 40 }}>
-          <button onClick={() => setStart(true)}>Start Demo</button>
-        </div>
-      ) : (
-        <MeetingProvider
-          config={{
-            meetingId: "",
-            micEnabled: true,
-            webcamEnabled: true,
-            name: "Guest User"
-          }}
-          token={TOKEN}
-        >
-          <Room />
-        </MeetingProvider>
-      )}
-    </>
+    <div style={{ padding: 30 }}>
+      <h1>VideoSDK Relay Demo</h1>
+      <DualRoomManager />
+    </div>
   );
 }
